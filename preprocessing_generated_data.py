ac2a9f48e720dbd04a3ab1d170299db6b9ff298b
LeVietAnhHuy <lvietanhhuy@gmail.com>
2025-03-31 14:03:46 +0700

    separate data generation

--- preprocessing_generated_data.py
+++ preprocessing_generated_data.py
@@ -12,6 +12,23 @@ from commpy.channels import awgn
 import os
 from tqdm import tqdm
 
+num_sample = 10
+num_rx_antennas = 1
+num_frame = 1
+num_slot = 1
+signal_len = num_frame * 1228800 + 1
+test_data = []
+
+for sample_index in range(num_sample):
+    random_generated_sample = np.random.rand(num_rx_antennas, signal_len)
+    test_data.append(random_generated_sample)
+
+starting_symbol = []
+starting_symbol.append(np.zeros((num_rx_antennas, num_slot)) + 921600)
+ending_symbol = []
+ending_symbol.append(np.zeros((num_rx_antennas, num_slot)) + 972687)
+
+
 tap_powers_dB = np.array([-6.9, 0, -7.7, -2.5, -2.4, -9.9, -8.0, -6.6, -7.1, -13.0, -14.2, -16.0])
 
 tap_delays = np.array([0, 65, 70, 190, 195, 200, 240, 325, 520, 1045, 1510, 2595])
@@ -34,7 +51,7 @@ step_snr_dB = 5
 print('')
 print('-----------------Noise-----------------')
 print("TDLC300-100")
-print(f"AWGN: {start_snr_dB}dB -> {end_snr_dB - 1}dB, step = {step_snr_dB}")
+print(f"AWGN: {start_snr_dB}dB -> {end_snr_dB}dB, step = {step_snr_dB}")
 print('')
 
 all_preamble_arr = []
@@ -109,12 +126,12 @@ numSample_perFrame = numSample_persubframe * 10
 
 sum_num = 12
 
-non_preprocessing_dataset = []
+dataset = []
 num_sample_per_snr = 5000
 num_sample_target_preamble_index = 1000
 target_preamble_index = 60
 
-print('\nGenerating non-preprocessing data:')
+print('Generating data:')
 for snr_dB in tqdm(snr_dB_range):
     num_sample = 1
 
@@ -127,70 +144,66 @@ for snr_dB in tqdm(snr_dB_range):
         # received_test_signal = awgn(all_preamble_arr[preamble_index], snr_dB=snr_dB)
         # received_test_signal = tdlChannel.corrupt_data(received_test_signal)
 
+
         received_test_signal = tdlChannel.corrupt_data(all_preamble_arr[preamble_index])
+        received_test_signal = awgn(received_test_signal, snr_dB=snr_dB)
 
         for antenna_index in range(num_rx_antennas):
-            received_test_signal[antenna_index, :] = awgn(received_test_signal[antenna_index, :], snr_dB=snr_dB)
+            single_antenna_signal = received_test_signal[antenna_index, :]
 
-        labels = np.zeros((num_rx_antennas, 1)) + preamble_index
-        received_test_signal = np.append(received_test_signal, labels, axis=1)
+            for frame_index in range(carrier_config.numFrame):
 
-        non_preprocessing_dataset.append(received_test_signal)
-        
-        if num_sample == 1:
-            print('')
+                preamble_slot = []
+                freq_comb_fft_pre_arr = []
+                for slot_index in range(all_preamble_start_mapping_symbol_arr[preamble_index].shape[1]):
+                    start_mapping_symbol = all_preamble_start_mapping_symbol_arr[preamble_index][frame_index, slot_index]
+                    end_mapping_symbol = all_preamble_end_mapping_symbol_arr[preamble_index][frame_index, slot_index]
 
-        print(f"{num_sample}/{num_sample_per_snr}, Preamble Index = {preamble_index}, {snr_dB}dB/{end_snr_dB - 1}dB")
+                    single_slot_signal = single_antenna_signal[start_mapping_symbol:(end_mapping_symbol + 1)]
+                    single_slot_signal_without_cp = single_slot_signal[prach_ofdm_information.cyclicPrefixLen:]
+                    single_slot_signal_arr = np.reshape(single_slot_signal_without_cp, (random_access_config.prachDuration, -1))
+                    single_slot_signal_fft_arr = fft(single_slot_signal_arr)
 
-        num_sample += 1
+                    multiplier = int(random_access_config.prachDuration / sum_num)
+                    num_col = single_slot_signal_fft_arr.shape[1]
+                    signal_fft_multiplier = np.reshape(single_slot_signal_fft_arr, (multiplier, sum_num, num_col))
 
-non_preprocessing_dataset_np = np.array(non_preprocessing_dataset)
-all_preamble_start_mapping_symbol_arr_np = np.array(all_preamble_start_mapping_symbol_arr)
-all_preamble_end_mapping_symbol_arr_np = np.array(all_preamble_end_mapping_symbol_arr)
+                    for multiple_index in range(multiplier):
+                        avg_freq_signal = np.sum(signal_fft_multiplier[multiple_index, :, :], axis=0) / sum_num
+                        avg_freq_signal_fftshift = fftshift(avg_freq_signal)
 
+                        freq_sequence = avg_freq_signal_fftshift[PrachStartingResourceElementIndex_freqDomain:(PrachStartingResourceElementIndex_freqDomain + random_access_config.L_RA)]
+                        freq_sequence = np.append(freq_sequence, preamble_index)   # label
+                        dataset.append(freq_sequence)
+
+                        print(f"\n{num_sample}/{num_sample_per_snr}, {snr_dB}dB")
+
+                        num_sample += 1
+
+dataset_np = np.array(dataset)
 print('')
 print('-----------------Preamble Data shape-----------------')
-print(f"preamble_arr_shape = {non_preprocessing_dataset_np.shape}")
+print(f"preamble_arr_shape = {dataset_np.shape}")
 
 generated_data_dir = 'generated_dataset'
 config_data_dir = 'pi_' + str(prach_config.preambleIndex) + \
-                   '_pci_' + str(prach_config.prachConfigurationIndex) + \
-                   '_rsi_' + str(prach_config.rootSequenceIndex) + \
-                   '_prscs_' + str(prach_config.subcarrierSpacing) + \
-                   '_puscs_' + str(carrier_config.subcarrierSpacing) + \
-                   '_zczc_' + str(prach_config.zeroCorrelationZoneConfig) + \
-                   '_fr_' + prach_config.frequencyRange + \
-                   '_s_' + prach_config.set + \
-                   '_st_' + prach_config.spectrumType + \
-                   '_fs_' + str(prach_config.frequencyStart) + \
-                   '_snrRange_' + str(start_snr_dB) + '_' + str(end_snr_dB) + '_' + str()
+                       '_pci_' + str(prach_config.prachConfigurationIndex) + \
+                       '_rsi_' + str(prach_config.rootSequenceIndex) + \
+                       '_prscs_' + str(prach_config.subcarrierSpacing) + \
+                       '_puscs_' + str(carrier_config.subcarrierSpacing) + \
+                       '_zczc_' + str(prach_config.zeroCorrelationZoneConfig) + \
+                       '_fr_' + prach_config.frequencyRange + \
+                       '_s_' + prach_config.set + \
+                       '_st_' + prach_config.spectrumType + \
+                       '_fs_' + str(prach_config.frequencyStart) + \
+                       '_snrRange_' + str(start_snr_dB) + '_' + str(end_snr_dB) + '_' + str()
 
 config_data_path = os.path.join(generated_data_dir, config_data_dir)
-if not os.path.exists(config_data_path):
-    os.makedirs(config_data_path)
-
-non_preprocessing_data_dir = 'non_preprocessing_dataset'
-non_preprocessing_data_path = os.path.join(config_data_path, non_preprocessing_data_dir)
-if not os.path.exists(non_preprocessing_data_path):
-    os.makedirs(non_preprocessing_data_path)
-
-dataset_name = 'non_preprocessing_rx_' + str(num_rx_antennas) + '_numFrame_' + str(carrier_config.numFrame) + '.npy'
-dataset_dir = os.path.join(non_preprocessing_data_path, dataset_name)
-
-starting_symbol_arr_name = 'rx_' + str(num_rx_antennas) + '_numFrame_' + str(carrier_config.numFrame) +'_starting_symbol_arr.npy'
-starting_symbol_arr_dir = os.path.join(non_preprocessing_data_path, starting_symbol_arr_name)
-
-ending_symbol_arr_name = 'rx_' + str(num_rx_antennas) + '_numFrame_' + str(carrier_config.numFrame) +'_ending_symbol_arr.npy'
-ending_symbol_arr_dir = os.path.join(non_preprocessing_data_path, ending_symbol_arr_name)
+os.makedirs(config_data_path, exist_ok=True)
+dataset_name = 'rx_' + str(num_rx_antennas) + '_freqComb_' + str(sum_num)  + '_numFrame_' + str(carrier_config.numFrame) + '.npy'
+dataset_dir = os.path.join(config_data_path, dataset_name)
 
 print('')
 print(f"Saving data to {dataset_dir}...")
-np.save(dataset_dir, non_preprocessing_dataset_np)
-
-print(f"Saving data to {starting_symbol_arr_dir}...")
-np.save(starting_symbol_arr_dir, all_preamble_start_mapping_symbol_arr_np)
-
-print(f"Saving data to {ending_symbol_arr_dir}...")
-np.save(ending_symbol_arr_dir, all_preamble_end_mapping_symbol_arr_np)
-
+np.save(dataset_dir, dataset_np)
 print('Done!')
